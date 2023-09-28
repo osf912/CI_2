ebcb0c7f6a98a14de362421df6005ee7a926dbc0
osf912 <123300938+osf912@users.noreply.github.com>
2023-09-28 22:50:27 +0200

    solved some minor problems

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const winSchemes = [
 [2,4,6]
 ];
 
-let gameStatus = ["", "", "", "", "", "", "", "", ""];
+let gameStatus = [ "", "", "", "", "", "", "", "", "" ];
 
 // Wait for the DOM to be loaded, then start game
 document.addEventListener("DOMContentLoaded", startGame)
@@ -19,9 +19,10 @@ document.addEventListener("DOMContentLoaded", startGame)
  */
 function startGame() {
 
-  //Add Eventlistener for fields on gameboard
   let fields = document.getElementsByClassName("fields");
   let rButton = document.getElementById("resetButton");
+
+  //Add Eventlistener for fields on gameboard
 
   for (let field of fields) {
     let fieldId = field.getAttribute("id");
@@ -34,7 +35,8 @@ function startGame() {
         gameStatus[fieldId] = currentPlayer;
         switchPlayer(currentPlayer); 
       } else {
-        alert("Field allready played!")
+        console.log(gameStatus[fieldId]);
+        alert("Box allready played!")
       };
 
       checkWin();
@@ -46,13 +48,24 @@ function startGame() {
     document.getElementById("xWon").innerText = "0";
     document.getElementById("oWon").innerText = "0";
   
-    let fields = document.getElementsByClassName("fields");
-    for (let field of fields){
-      field.innerText = "";
-    }
-  
-    gameStatus = ["", "", "", "", "", "", "", "", ""];
+    resetGameboard();
   });
+}
+
+/**
+ * Reset Gameboard and -status, First move X
+ */
+function resetGameboard() {
+
+  let fields = document.getElementsByClassName("fields");
+  
+  for (let field of fields){
+    field.innerText = "";
+  }
+
+  gameStatus = [ "", "", "", "", "", "", "", "", "" ];
+
+  document.getElementById("currentPlayer").innerText = "X";
 }
 
 /**
@@ -77,43 +90,52 @@ function switchPlayer(cPlayer) {
  */
 function checkWin() {
 
-    let somebodyWon = false;
+    console.log(gameStatus);
 
-    for (let i=0; i<9; i++) {
-      let field1 = gameStatus[winSchemes[i][0]];
-      let field2 = gameStatus[winSchemes[i][1]];
-      let field3 = gameStatus[winSchemes[i][2]];
+    for (let i=0; i<8; i++) {
+      let winScheme = winSchemes[i];
+      let field1 = gameStatus[winScheme[0]];
+      let field2 = gameStatus[winScheme[1]];
+      let field3 = gameStatus[winScheme[2]];
 
       if ((field1 === "") || (field2 === "") || (field3 === "")) {
         continue;
       }
 
       if ((field1 === field2) && (field2 === field3)) {
-        
+ 
         if (field1 === "X") {
           incXWon();
         } else {
           incOWon();
         };
+        resetGameboard();
         break;
       };
+
+      if(!gameStatus.includes("")) {
+        alert("Draw... No winner!");
+        resetGameboard();
+      }
     };
 }
 
 /**
- * Increment won-counter of X
+ * Increment won-counter of X and alert X won
  */
 function incXWon() {
   let count = parseInt(document.getElementById("xWon").innerText);
   count++;
   document.getElementById("xWon").innerText = count.toString(); 
+  alert("X won!");
 }
 
 /**
- * Increment won-counter of O
+ * Increment won-counter of O and alert O won
  */
 function incOWon() {
   let count = parseInt(document.getElementById("oWon").innerText);
   count++;
-  document.getElementById("oWon").innerText = count.toString(); 
+  document.getElementById("oWon").innerText = count.toString();
+  alert("O won!");
 }
